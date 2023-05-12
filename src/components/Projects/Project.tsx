8fb1f977fc6b1be9393100b5a2b898eea3c33b0e
Jakub Królicki <jakub.krolicki.k@gmail.com>
2023-05-12 19:06:56 +0200

    slider

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -1,28 +1,67 @@
 import { projectType } from './Projects'
-import { useState, useEffect } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import './Projects.css'
 
 export const Project = ({project} : {project : projectType}) => {
     const [slidePhoto, setSlidePhoto] = useState(0)
+    const slidePhotoRef = useRef(0)
+    const containerRef = useRef<HTMLDivElement | null>(null)
+    const observerRef = useRef<IntersectionObserver | null>(null)
 
-    useEffect(()=>{
-        setInterval(()=>{
-            
-        },3000)
 
-        return(()=>{
-            
+    useEffect(() => {
+        observerRef.current = new IntersectionObserver((entries) => {
+          if (entries[0].isIntersecting) {
+            startInterval()
+          }
         })
-    },[])
+
+        if(containerRef.current)
+            observerRef.current.observe(containerRef.current)
+    
+        return () => {
+          if (observerRef.current) {
+            observerRef.current.disconnect()
+          }
+        }
+      }, [])
+    
+      const startInterval = () => {
+        const interval = setInterval(() => {
+          if (slidePhotoRef.current === project.images.length - 1) {
+            slidePhotoRef.current = 0
+          } else {
+            slidePhotoRef.current += 1
+          }
+    
+          setSlidePhoto(slidePhotoRef.current)
+        }, 3000)
+    
+        return () => {
+          clearInterval(interval)
+        }
+      }
 
     return(
         <div className='project'>
             <h2>{project.title}</h2>
-            <div className='project-images-slider'>
-                {project.images.map((image, index) =>{
-                    return <img alt={image} src={`/projectsImages/${image}`} key={index}/>
-                })}
+            <div className='project-images-slider' ref={containerRef}>
+                <span className='project-images' style={{transform: `translateX(-${slidePhoto * 100}%)`}}>
+                    {project.images.map((image, index) =>{
+                        return <img alt={image} src={`/projectsImages/${image}`} key={index}/>
+                    })}
+                </span>
             </div>
+            <span className='project-slider-dots'>
+                    {project.images.map((image, index) => (
+                        <span
+                            key={index}
+                            className={`dot ${index === slidePhoto ? "active-dot" : ""}`}
+                            onClick={() => setSlidePhoto(index)}
+                        />
+                        ))
+                    }
+            </span>
             <p>{project.description}</p>
             <span className='link'>
                 <a href={`${project.ref.link}`} target='_blank'><b>{project.ref.desc}</b></a>
