cda40f143477944adf7b0954afce64bd5361af2a
Jakub Królicki <jakub.krolicki.k@gmail.com>
2023-06-25 17:26:03 +0200

    Projects translations

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -14,77 +14,77 @@ export type projectType = {
     technologies: string[]
 }
 
-const projectsList : projectType[] = [
-    {
-        title: "Jedność Gorlice",
-        images: ["jednosc1.png", "jednosc2.png", "jednosc3.png", "jednosc4.png"],
-        description: 'Strona internetowa dla Spółdzielni Produkcyjno-Handlowej "JEDNOŚĆ" w Gorlicach.\nOpisuje historę firmy, zakłady produkcyjne, produkty piekarskie i cukiernicze oraz placówki.',
-        ref:{
-            desc: "Link do strony",
-            link: "https://www.jednosc.gorlice.pl/"
-        },
-        technologies: ["React", "JavaScript", "React Router", "React Helmet"]
-    },
-    {
-        title: "Forum",
-        images: ["forum1.png", "forum2.png","forum3.png","forum4.png","forum5.png"],
-        description: 'Strona "Landing Page" z forum.\nUmożliwia logowanie, rejestrację, dodawanie postów, edytowanie ich i usuwanie. Zawiera profile użytkowników ze statystykami, ranking najlepszych postów, wyszukiwarkę i podział postów na strony.\nTo mój najbardziej rozwinięty projekt.',
-        ref:{
-            desc: "Link do repozytorium",
-            link: "https://github.com/Krolicki/react-forum-page"
-        },
-        technologies: ["React", "JavaScript", "Firebase", "React Router"]
-    },
-    {
-        title: "Wielokrokowy Formularz",
-        images: ["step1.png", "step2.png", "step3.png"],
-        description: 'Formularz z walidacją pól (adres email, hasło, data urodzenia, zgody) i paskiem postępu.\nPosiada dwie wersje językowe: polski i angielski.',
-        ref:{
-            desc: "Link do repozytorium",
-            link: "https://github.com/Krolicki/react-multi-step-form"
-        },
-        technologies: ["React", "Typescript", "i18next"]
-    },
-    {
-        title: "Aplikacja Pogodowa",
-        images: ["weather.png"],
-        description: 'Aplikacja pogodowa z prognozą na 5 dni.',
-        ref:{
-            desc: "Link do repozytorium",
-            link: "https://github.com/Krolicki/react-weather"
-        },
-        technologies: ["React", "Typescript", "OpenWeather API"]
-    },
-    {
-        title: "Edytor zdjęć",
-        images: ["editor1.png", "editor2.png"],
-        description: 'Pozwala na edytowanie zdjęć filtrami CSS i zapisywanie plików przy użyciu HTML Canvas.\nZdjęcia można przesłać lub wybrać z niekończońcej się ściany zdjęć pobieranych z Unsplash.com.',
-        ref:{
-            desc: "Link do repozytorium",
-            link: "https://github.com/Krolicki/react-photo-editor"
-        },
-        technologies: ["React", "Typescript", "HTML Canvas"]
-    },
-    {
-        title: "Raspberry Pi",
-        images: ["rasp1.png", "rasp2.png", "rasp3.png"],
-        description: 'Tworzę także aplikacje i systemy na Raspberry Pi.\nWykonałem m.in. sterowanie roletami okiennymi, sterowanie światłem, system alarmowy z czujnikami ruchu, czujnikiem otwarcia drzwi i kartami dostępu.',
-        ref:{
-            desc: "Link do repozytoriów",
-            link: "https://github.com/Krolicki?tab=repositories&q=raspberry&type=&language=&sort="
-        },
-        technologies: ["Python", "Raspberry Pi", "Flask", "Javascript"]
-    }
-]
-
 export const Projects = forwardRef<HTMLDivElement>((_,ref) => {
     const [photoToShow, setPhotoToShow] = useState<string | null>(null)
 
     const { t } = useTranslation()
 
+    const projectsList : projectType[] = [
+        {
+            title: t('projects.project.jednosc.title'),
+            images: ["jednosc1.png", "jednosc2.png", "jednosc3.png", "jednosc4.png"],
+            description: t('projects.project.jednosc.description'),
+            ref:{
+                desc: t('projects.ref.website'),
+                link: "https://www.jednosc.gorlice.pl/"
+            },
+            technologies: ["React", "JavaScript", "React Router", "React Helmet"]
+        },
+        {
+            title: t('projects.project.forum.title'),
+            images: ["forum1.png", "forum2.png","forum3.png","forum4.png","forum5.png"],
+            description: t('projects.project.forum.description'),
+            ref:{
+                desc: t('projects.ref.repo'),
+                link: "https://github.com/Krolicki/react-forum-page"
+            },
+            technologies: ["React", "JavaScript", "Firebase", "React Router"]
+        },
+        {
+            title: t('projects.project.multiStepForm.title'),
+            images: ["step1.png", "step2.png", "step3.png"],
+            description: t('projects.project.multiStepForm.description'),
+            ref:{
+                desc: t('projects.ref.repo'),
+                link: "https://github.com/Krolicki/react-multi-step-form"
+            },
+            technologies: ["React", "Typescript", "i18next"]
+        },
+        {
+            title: t('projects.project.weatherApp.title'),
+            images: ["weather.png"],
+            description: t('projects.project.weatherApp.description'),
+            ref:{
+                desc: t('projects.ref.repo'),
+                link: "https://github.com/Krolicki/react-weather"
+            },
+            technologies: ["React", "Typescript", "OpenWeather API"]
+        },
+        {
+            title: t('projects.project.photoEditor.title'),
+            images: ["editor1.png", "editor2.png"],
+            description: t('projects.project.photoEditor.description'),
+            ref:{
+                desc: t('projects.ref.repo'),
+                link: "https://github.com/Krolicki/react-photo-editor"
+            },
+            technologies: ["React", "Typescript", "HTML Canvas"]
+        },
+        {
+            title: t('projects.project.raspberryPi.title'),
+            images: ["rasp1.png", "rasp2.png", "rasp3.png"],
+            description: t('projects.project.raspberryPi.description'),
+            ref:{
+                desc: t('projects.ref.repo'),
+                link: "https://github.com/Krolicki?tab=repositories&q=raspberry&type=&language=&sort="
+            },
+            technologies: ["Python", "Raspberry Pi", "Flask", "Javascript"]
+        }
+    ]
+
     return(
         <section className='projects-wraper' ref={ref}>
-            <h1>{t('projects.title')}</h1>
+            <h1>{t('projects.header')}</h1>
             {projectsList.map((project, index)=>{
                 return(
                     <Project project={project} flip={index % 2 === 1} key={project.title} setPhoto={setPhotoToShow}/>
