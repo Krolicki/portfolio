217c6629a9d2cdb8fcedef4a30a91cf0d5f1317d
Jakub Królicki <jakub.krolicki.k@gmail.com>
2023-06-25 17:40:05 +0200

    github and contact translations

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,13 +1,16 @@
 import { forwardRef } from 'react'
 import './Contact.css'
 import { ReactComponent as MailIcon } from './mail.svg'
+import { useTranslation } from 'react-i18next';
 
 export const Contact = forwardRef<HTMLDivElement>((_,ref) => {
+    const { t } = useTranslation()
+
     return(
         <section className='contact-wraper' ref={ref}>
             <div className='contact-content'>
-                <h2>Poznamy się?</h2>
-                <p>Jeśli zainteresowała Cię moja praca, możesz skontaktować się za mną mailowo, lub odwiedzić mój profil na <a href="https://www.linkedin.com/in/jakub-krolicki/" target='_blank' className='contact-linkedin'>LinkedIn</a></p>
+                <h2>{t('contact.header')}</h2>
+                <p>{t('contact.content')}<a href="https://www.linkedin.com/in/jakub-krolicki/" target='_blank' className='contact-linkedin'>LinkedIn</a></p>
                 <a href="mailto:jakub.krolicki.k@gmail.com">jakub.krolicki.k@gmail.com</a><br />
             </div>
             <div className='contact-icon-wraper'>
--- src/components/GitHub/GitHub.tsx
+++ src/components/GitHub/GitHub.tsx
@@ -1,12 +1,15 @@
 import { forwardRef } from 'react'
 import './GitHub.css'
 import {ReactComponent as GitLogo} from './github-mark-white.svg'
+import { useTranslation } from 'react-i18next';
 
 export const GitHub = forwardRef<HTMLDivElement>((_,ref) => {
+    const { t } = useTranslation()
+
     return (
         <section className='github-wraper' ref={ref}>
             <div className='git-info'>
-                <h3>Sprawdź wszystkie moje projekty i to nad czym aktualnie pracuję na</h3>
+                <h3>{t('github')}</h3>
                 <a href="https://github.com/Krolicki" target='_blank'><img src="GitHub_Logo_White.png" /></a>
             </div>
             <div className='git-logo-wraper'>
